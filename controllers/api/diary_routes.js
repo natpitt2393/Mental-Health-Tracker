0635a2630e5ca59d75830f8a1e91077531e5022b
trealtye20 <exantustyrese10@gmail.com>
2021-12-16 12:05:23 -0500

    index changes

---
+++ controllers/api/diary_routes.js
@@ -0,0 +1,48 @@
+// one route to get all diary entries GET /api/diary
+//one route to get a single entry GET /api/diary/:id
+// one route to submit a single entry POST /api/diary
+
+const router = require('express').Router();
+const { application } = require('express');
+const { Diary, Mood, User } = require('../..models');
+
+// GET all Diary
+// localhost:3001/api/diary
+router.get('/', async (req, res) => {
+  try {
+    const diaryData = await Diary.findAll({
+      include: [{ model: Mood }, { model: User }],
+    });
+    res.status(200).json(diaryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// localhost:3001/api/diary
+router.post('/', async (req, res) => {
+    const diaryData = await Diary.create(req.body);
+  
+    return res.json(diaryData);
+  });
+
+  
+  // GET a single Diary
+  // localhost:3001/api/diary/id
+  router.get('/:id', async (req, res) => {
+    try {
+    const diaryData = await Diary.findByPk(req.params.id, {
+      include: [{ model: Mood }, { model: User }],
+    });
+
+    if (!diaryData) {
+      res.status(404).json({ message: 'diary not found' });
+      return;
+    }
+    
+    res.status(200).json(diaryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const htmlRoutes = require('./html_routes');
+
+router.use('/', htmlRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
