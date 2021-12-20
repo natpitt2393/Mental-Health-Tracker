3171d2d8b63e3b87881e87bfd160167eba3de8d4
Anthony Jimenez <tony6776a@gmail.com>
2021-12-20 13:00:35 -0500

    Changes with prof

--- insert_user.sql
+++
@@ -1,3 +0,0 @@
-insert into user (name, email, password) values ("Al", "al@email.com", "pw_Al");
-insert into user (name, email, password) values ("Bee", "bee@email.com", "pw_Bee");
-insert into user (name, email, password) values ("Cindy", "cindy@email.com", "pw_Cindy");
