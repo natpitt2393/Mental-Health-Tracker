58ffba903ec111a3a48ab97e2018d3fcf4036a5d
Anthony Jimenez <tony6776a@gmail.com>
2021-12-20 22:30:13 -0500

    Added view entry box

--- seeds/diary-seeds.js
+++ seeds/diary-seeds.js
@@ -2,35 +2,35 @@ const Diary = require('../models/Diary');
 //test
 const diaryData = [
   {
-    title: 'Worstggggggggg Day Ever',
-    description: "descrip",
+    title: 'This the title',
+    description: "This is the description",
     mood_id: 1,
     user_id: 1,
     date_created: "12/12/1212"
   },
   {
-    title: 'Worstggggggggg Day Ever',
-    description: "descrip",
+    title: 'Title 2',
+    description: "This is the body of the description its full of text",
     mood_id: 3,
     user_id: 3,
     date_created: "12/12/1212"
   },
   {
-    title: 'Worstggggggggg Day Ever',
-    description: "descrip",
+    title: 'This is some other title',
+    description: "We are stuck on somethings",
     mood_id: 5,
     user_id: 2,
     date_created: "12/12/1212"
   },
   {
-    title: 'Worstggggggggg Day Ever',
-    description: "descrip",
+    title: 'This sucks',
+    description: "this is the body again of something",
     mood_id: 7,
     user_id: 3,
     date_created: "12/12/1212"
   },
   {
-    title: 'Worstggggggggg Day Ever',
+    title: 'I hate this',
     description: "descrip",
     mood_id: 9,
     user_id: 2,
